ce3838172ffd0c06a3f11c88c49c3fb28ca3d3c1
Amrinder <asin211@mywhitecliffe.com>
2022-11-16 22:02:48 +1300

    static root fixed

--- base/settings.py
+++ base/settings.py
@@ -149,7 +149,7 @@ MEDIA_ROOT = BASE_DIR / "static/images"
 
 # use this root for production
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
